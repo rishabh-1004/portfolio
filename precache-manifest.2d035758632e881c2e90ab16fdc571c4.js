ba9110f3d81bf8e26ec7f30646804fcb365c90a2
Rishabh <rishabhrajshrm00@gmail.com>
2020-08-30 08:37:17 +0530

    Updates

---
+++ precache-manifest.2d035758632e881c2e90ab16fdc571c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd466a29b5952802b6fb9bbb6d562faa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b09bb2a24845bdc1b96b",
+    "url": "/portfolio/static/css/main.d0a40a05.chunk.css"
+  },
+  {
+    "revision": "02a718cce1e7fb6e7a6f",
+    "url": "/portfolio/static/js/2.6bfc0409.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio/static/js/2.6bfc0409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09bb2a24845bdc1b96b",
+    "url": "/portfolio/static/js/main.02a54dd4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "509b6544736a115d41cad7919d9f8afe",
+    "url": "/portfolio/static/media/avatar.509b6544.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
